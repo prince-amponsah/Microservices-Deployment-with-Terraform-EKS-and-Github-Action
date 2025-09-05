2b5ae16116df5a4d8e48304d7d45516a186dfe52
Prince <princekwes@gmail.com>
2025-09-05 17:08:21 +0000

    backend Code Ready

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'localhost',
+   host: 'reactnodeapp.cjwuu8ucyn73.us-west-2.rds.amazonaws.com:3306',
    port: '3306',
-   user: 'root',
-   password: '12345678',
+   user: 'appuser',
+   password: 'learnIT02#',
    database: 'react_node_app'
 });
 
