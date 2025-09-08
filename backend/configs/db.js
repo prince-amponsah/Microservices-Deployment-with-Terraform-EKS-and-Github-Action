346c76e975e9d53dceadf1b2b23c78213a5c1ff6
Prince Amponsah <princekwes@gmail.com>
2025-09-08 07:30:36 +0000

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'reactnodeapp.cjwuu8ucyn73.us-west-2.rds.amazonaws.com:3306',
-   port: '3306',
-   user: 'appuser',
-   password: 'learnIT02#',
-   database: 'react_node_app'
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT || 3306,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME
 });
 
 module.exports = db;
